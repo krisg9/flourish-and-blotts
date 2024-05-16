61557f87d8c795937fe6bb412a486d7aec7db80c
Kristiyan <kristiyan.georgiev9@gmail.com>
2024-05-16 20:16:23 +0200

    post form has two columns

--- src/pages/PostForm.tsx
+++ src/pages/PostForm.tsx
@@ -1,10 +1,19 @@
-import { FormControl, TextField } from "@mui/material";
+import { FormControl, FormLabel, Grid, TextField } from "@mui/material";
 
 const PostForm = () => {
 	return (
 		<>
 			<FormControl>
-				<TextField type="text" size="medium" defaultValue="title"></TextField>
+				<Grid container spacing={2}>
+					<Grid container item xs={6} direction="column">
+						<FormLabel>Title</FormLabel>
+						<TextField type="text" size="medium" defaultValue=""></TextField>
+					</Grid>
+					<Grid container item xs={6} direction="column">
+						<FormLabel>Author</FormLabel>
+						<TextField type="text" size="medium" defaultValue=""></TextField>
+					</Grid>
+				</Grid>
 			</FormControl>
 		</>
 	);
