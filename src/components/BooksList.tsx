f7ff37a41ad2b2964d23a27e3f66350f18c84b0d
Kristiyan <kristiyan.georgiev9@gmail.com>
2024-05-19 21:45:22 +0200

    add publisher to book card

--- src/components/BooksList.tsx
+++ src/components/BooksList.tsx
@@ -18,7 +18,7 @@ const BooksList = () => {
 			try {
 				const fetchedBooks = await getAllBooks();
 				fetchedBooks.push({
-					title: "Random book",
+					title: "Cool random book",
 					subtitle: "Random subtitle",
 					isbn: "1234",
 					abstract: "Very cool random book",
