0167d91ae76d6f21ff6f607cdefefdefdcff3a40
Kristiyan <kristiyan.georgiev9@gmail.com>
2024-06-30 15:38:37 +0200

    add checkout page

--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -1,30 +1,27 @@
-import { Button, Paper, Typography } from "@mui/material";
+import { Box, Button, Paper, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
 const CheckoutPage = () => {
 	const { clearBasket } = useAuth();
 	return (
-		<Paper
-			elevation={3}
-			style={{ padding: "3em", minHeight: "50%", borderRadius: "2em", gap: 5 }}
-		>
-			<Typography variant="h2" gutterBottom>
-				Thank You for Your Order!
-			</Typography>
-			<Typography variant="h5" gutterBottom>
-				Your order has been placed successfully. You will receive an email
-				confirmation shortly.
-			</Typography>
-			<Link to={"/"}>
-				<Button
-					variant="contained"
-					color="primary"
-					onClick={() => clearBasket()}
-				>
-					Continue Shopping
-				</Button>
-			</Link>
+		<Paper elevation={3} sx={{ padding: "3em", borderRadius: "2em" }}>
+			<Box sx={{ display: "flex", flexDirection: "column", gap: "2em" }}>
+				<Typography variant="h2">Thank You for Your Order!</Typography>
+				<Typography variant="h5">
+					Your order has been placed successfully. You will receive an email
+					confirmation shortly.
+				</Typography>
+				<Link to={"/"}>
+					<Button
+						variant="contained"
+						color="primary"
+						onClick={() => clearBasket()}
+					>
+						Continue Shopping
+					</Button>
+				</Link>
+			</Box>
 		</Paper>
 	);
 };
